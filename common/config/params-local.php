5ca63cbd90a6708f9e152b514ec970fb74fe47c6
Raffaele Mancino <raffaele.linux@gmail.com>
2018-09-28 19:55:44 +0200

    kartik gridview config

---
+++ common/config/params-local.php
@@ -0,0 +1,8 @@
+<?php
+return [
+        'GoogleDeveloperKey' => 'YOUR_DEVELOPER_KEY',
+        // See http://code.google.com/p/google-api-php-client/wiki/OAuth2
+        //'GoogleClientId' => 'YOUR_CLIENT_ID',
+        //'GoogleClientSecret' => 'YOUR_CLIENT_SECRET',
+        //'GoogleRedirectUri' => 'YOUR_REDIRECT_URI',
+];
---
+++ frontend/config/main-local.php
@@ -0,0 +1,22 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'nAYFnuFdht_JesVXt3RWnDhGMfH6XrId',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+    $config['modules']['gridview'] = ['class' => '\kartik\grid\Module'];
+}
+
+return $config;
